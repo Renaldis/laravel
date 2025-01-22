1dd69f4f0c1e621143a530cc36e0ef2d9042e4a5
Renaldis <renaldis142@gmail.com>
2025-01-22 12:52:24 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+$movies = [];
+
+for ($i=0; $i < 10; $i++) { 
+    $movies[] = [
+        'title' => "movie ". $i,
+        'year' => "2022",
+        'genre' => "action" 
+    ];
+}
+
+Route::get('/movie',function() use($movies){
+    echo "<h1>Welcome</h1>";
+    echo "<ul>";
+    foreach ($movies as $movie) {
+        echo "<li>" . $movie['title'] . " - ". $movie["year"] . " - " . $movie['genre'] . "</li>";
+    }
+    echo "</ul>";
+});
+
+Route::post('/movie', function() use($movies){
+    $movies[]=[
+        'title' => request('title'),
+        'year' => request('year'),
+        'genre' => request('genre'),
+    ];
+
+    echo "<h1>Welcome</h1>";
+    echo "<ul>";
+    foreach ($movies as $movie) {
+        echo "<li>" . $movie['title'] . " - ". $movie["year"] . " - " . $movie['genre'] . "</li>";
+    }
+    echo "</ul>";
+});
